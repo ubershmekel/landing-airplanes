f1ce0a641b1cb35e7f6717c7e6f719587bbcee16
Yuval Greenfield <ubershmekel@gmail.com>
2018-01-01 12:54:14 -0800

    Visual assets and cleanup

--- index.js
+++ index.js
@@ -18,54 +18,68 @@ Display:
 */
 
 
-function geoFindMe() {
-    var output = document.getElementById("out");
 
-    function outputText(text) {
-        var prevHtml = output.innerHTML;
-        output.innerHTML = '<p>' + text + '</p>' + prevHtml;
-    }
 
-    function geo_success(position) {
-        var latitude = position.coords.latitude;
-        var longitude = position.coords.longitude;
-        var altitude = position.coords.longitude;
-        var timestamp = new Date().toISOString();
+var output;
 
-        outputText('Latitude is ' + latitude
-            + ' <br/>Longitude is ' + longitude
-            + ' <br/>Altitude is ' + altitude
-            + ' <br/>Time is ' + timestamp
-        );
-
-        /*var img = new Image();
-        img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
-        output.appendChild(img);*/
-    }
-
-    function geo_error(err) {
-        output.innerHTML = "Unable to retrieve your location";
-        showAlert('Geo error - ' + err.code + ' - ' + err.message);
-    }
-
-    function main() {
-        if (!navigator.geolocation){
-            outputText("Geolocation is not supported by your browser");
-            return;
-        }
-
-        outputText("Locating...123");
-        var geo_options = {
-            enableHighAccuracy: false,
-            timeout: 1000,
-            maximumAge: 1000,
-        };
-
-        navigator.geolocation.watchPosition(geo_success, geo_error, geo_options);
-    }
-
-    main();
+function outputText(text) {
+    var prevHtml = output.innerHTML;
+    output.innerHTML = '<p>' + text + '</p>' + prevHtml;
 }
 
-exports.geoFindMe = geoFindMe;
-//exports.geoToXyz = geoToXyz;
+function geoSuccess(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    var altitude = position.coords.longitude;
+    var timestamp = new Date().toISOString();
+
+    outputText('Latitude is ' + latitude
+        + ' <br/>Longitude is ' + longitude
+        + ' <br/>Altitude is ' + altitude
+        + ' <br/>Time is ' + timestamp
+    );
+
+    /*var img = new Image();
+    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+    output.appendChild(img);*/
+}
+
+function geoError(err) {
+    output.innerHTML = "Unable to retrieve your location";
+    showAlert('Geo error - ' + err.code + ' - ' + err.message);
+}
+
+function setupLog() {
+    output = document.getElementById("log");
+}
+
+function watchPosition() {
+    if (!navigator.geolocation){
+        outputText("Geolocation is not supported by your browser");
+        return;
+    }
+
+    outputText("Locating...123");
+    var geoOptions = {
+        enableHighAccuracy: false,
+        timeout: 10000,
+        maximumAge: 5000,
+    };
+
+    navigator.geolocation.watchPosition(geoSuccess, geoError, geoOptions);
+}
+
+function setupButtons() {
+    document.getElementById('start-button').onclick = watchPosition;
+}
+
+function main() {
+    setupLog();
+    setupButtons();
+}
+
+if (document.readyState === 'complete' || document.readyState !== 'loading') {
+    main();
+} else {
+    document.addEventListener('DOMContentLoaded', main);
+}
